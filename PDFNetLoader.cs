224bb52a76c35c45c7bf2bf353ffc0bb0ac5ee4c
rbarrpdftron <rbarr@pdftron.com>
2014-08-26 11:14:56 -0700

    Initial commit of PDFNetLoader code

---
+++ PDFNetLoader.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Reflection;
+
+namespace pdftron
+{
+    /// <summary>
+    /// This class allows for AnyCPU support, and loads the corresponding
+    /// x86 or x64 PDFNet.dll at runtime based on the current system. Also
+    /// known as side-by-side assembly loading.
+    /// </summary>
+    /// <example>
+    /// To use this class, the method PDFNetLoader.Instance must be added before
+    /// any methods containing calls to PDFNet are called. The easiest way to handle
+    /// this, is to add the following line of code to the class containing your
+    /// entry point, or Main() method.
+    /// <code>
+    /// private static pdftron.PDFNetLoader pdfnet = pdftron.PDFNetLoader.Instance();
+    /// </code>
+    /// </example>
+    public class PDFNetLoader
+    {
+        #region Interface
+
+        /// <summary>
+        /// Load PDFNetLoader for runtime PDFNet loading.
+        /// Loads PDFNet.dll's from 'PDFNet/[x86|x64]'
+        /// folders in current working directory.
+        /// </summary>
+        /// <returns></returns>
+        public static PDFNetLoader Instance()
+        {
+            return _singleton;
+        }
+
+        /// <summary>
+        /// Change the assembly loading directory. Default is 'PDFNet' sub-folder
+        /// in the current working directory. Target folder needs to contain
+        /// two sub-folders, x86 and x64, each containing the respective PDFNet.dll assembly.
+        /// </summary>
+        /// <param name="path">The new path to look for the x86 and x64 folders containing PDFNet.dll</param>
+        /// <returns>The PDFNetLoader instance</returns>
+        public PDFNetLoader Path(string path)
+        {
+            this.path = path;
+            return this;
+        }
+
+        # endregion
+
+        #region Implementation
+
+
+        private Assembly PDFNetResolveEventHandler(object sender, ResolveEventArgs args)
+        {
+            if (!args.Name.StartsWith("PDFNet,")) return null;
+            
+            Assembly assembly = null;
+            string module_path = path;
+            if (Is64BitProcess())
+            {
+                module_path = System.IO.Path.Combine(module_path, "x64");
+            }
+            else
+            {
+                module_path = System.IO.Path.Combine(module_path, "x86");
+            }
+            module_path = System.IO.Path.Combine(module_path, "PDFNet.dll");
+            assembly = Assembly.LoadFrom(module_path);
+            return assembly;
+        }
+
+        private PDFNetLoader()
+        {
+            path = "PDFNet";
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.AssemblyResolve += new ResolveEventHandler(PDFNetResolveEventHandler);
+        }
+
+        private static bool Is64BitProcess()
+        {
+            return IntPtr.Size == 8;
+        }
+
+        private string path;
+
+        private static readonly PDFNetLoader _singleton = new PDFNetLoader();
+
+        #endregion
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PDFNetLoader")]
+[assembly: AssemblyDescription("Dynamically load 32 or 64 bit PDFNet assembly at runtime.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PDFTron")]
+[assembly: AssemblyProduct("PDFNetLoader")]
+[assembly: AssemblyCopyright("Copyright © PDFTron Systems Inc. 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ac70c126-3a69-448e-840d-7d81c68f0f3c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
